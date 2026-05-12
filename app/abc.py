28caf173a3329d48d63aaeef1da9d056e338872b
ranj7645 <ranjeetkumar70337@gmail.com>
2026-05-12 18:36:27 +0530

    first ci cd push

---
+++ app/abc.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self,balance=0):
+        self.balance = balance
+
+    def deposit(self,amount):
+        self.balance += amount
+
+    def withdraw(self,amount):
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *=0.1
